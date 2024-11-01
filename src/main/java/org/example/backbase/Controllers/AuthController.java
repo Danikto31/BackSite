de5a80dee08655812b23c8bfbc60aa79211c04dd
Арсений <huycuka9@gmail.com>
2024-11-01 17:03:07 +0300

    0.0.1

--- src/main/java/org/example/backbase/Controllers/AuthController.java
+++ src/main/java/org/example/backbase/Controllers/AuthController.java
@@ -1,6 +1,7 @@
 package org.example.backbase.Controllers;
 
 import org.example.backbase.Entity.BuyerClient;
+import org.example.backbase.Entity.SellerClient;
 import org.example.backbase.Services.ClientService;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,17 +16,29 @@ public class AuthController {
     @Autowired
     private ClientService clientService;
 
-    @PostMapping("/register")
+    @PostMapping("/registerClient")
     public ResponseEntity<String> registerClient(@RequestBody BuyerClient buyerClient) {
         try {
             // Сохраняем пользователя
             clientService.saveClient(buyerClient.getUsername(), buyerClient.getPassword());
-            return ResponseEntity.ok("Регистрация прошла успешно");
+            return ResponseEntity.ok("Регистрация клиента прошла успешно");
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Пользователь уже существует");
+            return ResponseEntity.badRequest().body("Пользователь(Клиент) уже существует");
         }
     }
 
+    @PostMapping("/registerSeller")
+    public ResponseEntity<String> registerSeller(@RequestBody SellerClient sellerClient){
+        try {
+
+            clientService.saveClient(sellerClient.getUsername(),sellerClient.getPassword());
+            return ResponseEntity.ok("Регистрия продавца прошла успешно");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Пользователь(Продавец) уже существует");
+        }
+    }
+
+    @PostMapping
     public boolean checkClient(@RequestBody String name){
         return clientService.findByUsername(name) != null;
     }
